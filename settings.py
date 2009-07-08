a2514ce6eff100fa1af864f9defe41ff3ed80a43
Eric Redmond <eric.redmond@gmail.com>
2009-07-08 19:40:46 -0400

    added prod

--- settings.py
+++ settings.py
@@ -7,7 +7,16 @@ SANDBOX = {
   'google-co-username':'875093275712045',
   'google-co-password':'qNztxsdPtwA9f_dICQmjhg',
   'email-sender':'eric.redmond@gmail.com',
-  'email-subject':'Someone gave you a task',
+  'email-subject':'[MT:SAND] Someone gave you a task',
+  'email-to':'Jim Wilson <wilson.jim.r@gmail.com>, Eric Redmond <eric.redmond@gmail.com>',
+}
+
+PRODUCTION = {
+  'google-co':'https://checkout.google.com/api/checkout/v2/checkout/Merchant/363339485026092',
+  'google-co-username':'363339485026092',
+  'google-co-password':'fXvOLxZNLf7_NXmgQewUVQ',
+  'email-sender':'eric.redmond@gmail.com',
+  'email-subject':'[MT] Someone gave you a task',
   'email-to':'Jim Wilson <wilson.jim.r@gmail.com>, Eric Redmond <eric.redmond@gmail.com>',
 }
 
